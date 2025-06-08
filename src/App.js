7ff1e9aad4c68164f87bb2367c3851cdbd39fc70
altprojekt <persdive@gmail.com>
2025-06-08 21:34:58 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Markhus Byggmester Kvalitet</h1>
+      <p>Velkommen! Her kan du laste opp bilder av riktig og feil utførte arbeider.</p>
+    </div>
+  );
+}
+
+export default App;
